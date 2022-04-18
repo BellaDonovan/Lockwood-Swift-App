ddfc1ab4c754f067512dfb14f9dad622e042bf84
Bella Donovan <belladonovan@unomaha.edu>
2022-04-18 17:58:47 -0500

    Initial Commit

---
+++ Life Long Learning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Life Long Learning
+//
+//  Created by Swift Team on 4/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
