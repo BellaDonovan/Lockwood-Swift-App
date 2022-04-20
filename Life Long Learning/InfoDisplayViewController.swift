5f81df8afaa65daa35591263386e246ac3214a55
BellaDonovan <belladonovan@unomaha.edu>
2022-04-20 10:37:05 -0500

    Created the UI and connected the continue button to the info screen (segue). Created the InfoDisplayViewController that corresponds to the second screen. The large grey square is the WebViewKit

---
+++ Life Long Learning/InfoDisplayViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InfoDisplayViewController.swift
+//  Life Long Learning
+//
+//  Created by Swift Team on 4/20/22.
+//
+
+import UIKit
+
+class InfoDisplayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
